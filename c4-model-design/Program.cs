06487958debe5c87824a5ad1314a5355ee1da518
DominikMendoza <u20201b980@upc.edu.pe>
2022-10-24 11:19:39 -0500

    feat(Program): Added new software systems and redistribution of  context's

--- c4-model-design/Program.cs
+++ c4-model-design/Program.cs
@@ -12,9 +12,9 @@ namespace c4_model_design
 
         static void RenderModels()
         {
-            const long workspaceId = 77391;
-            const string apiKey = "05545864-98fa-426b-a228-3e340763f5f4";
-            const string apiSecret = "4fb1d5e4-4acb-4189-ba87-c0ccc3b3df63";
+            const long workspaceId = 77525;
+            const string apiKey = "92cd0cb3-a737-4353-b4b4-ba0a01ea868b";
+            const string apiSecret = "2d31febd-162b-4e11-8423-70cdc8b3bda0";
 
             StructurizrClient structurizrClient = new StructurizrClient(apiKey, apiSecret);
 
@@ -30,6 +30,10 @@ namespace c4_model_design
             SoftwareSystem Plin = model.AddSoftwareSystem("Plin", "Permite realizar transacciones monetarias simples a través de un código QR");
             SoftwareSystem Tunki = model.AddSoftwareSystem("Tunki", "Permite realizar transacciones monetarias simples a través de un código QR");
             SoftwareSystem Visa = model.AddSoftwareSystem("Visa", "Permite realizar transacciones monetarias de una cuenta a otra");
+            SoftwareSystem GoogleMeet = model.AddSoftwareSystem("Google Meet", "Permite realizar videoconferencias");
+            SoftwareSystem GoogleCalendar  = model.AddSoftwareSystem("Google Calendar", "Permite agendar citas");
+            SoftwareSystem GoogleAccount = model.AddSoftwareSystem("Google Account", "Permite gestionar cuentas de correo electrónico");
+            SoftwareSystem ChatBot = model.AddSoftwareSystem("ChatBot", "Permite interactuar con el paciente de forma natural");
 
             Person Paciente = model.AddPerson("Paciente", "Paciente con algún problema de salud mental");
             Person Psicologo = model.AddPerson("Psicólogo", "¨Profesional de la salud mental");
@@ -41,34 +45,36 @@ namespace c4_model_design
             Enlazador.Uses(Plin, "Consulta información en tiempo real el estado y las transacciones de la cuenta de la aplicación, y paga a los psicólogos");
             Enlazador.Uses(Tunki, "Consulta información en tiempo real el estado y las transacciones de la cuenta de la aplicación, y paga a los psicólogos");
             Enlazador.Uses(Visa, "Consulta información en tiempo real el estado y las transacciones de la cuenta de la aplicación, y paga a los psicólogos");
-
-            Paciente.Uses(Yape, "Consulta información de su estado de cuenta y hace transferencias a la aplicación");
-            Paciente.Uses(Plin, "Consulta información de su estado de cuenta y hace transferencias a la aplicación");
-            Paciente.Uses(Tunki, "Consulta información de su estado de cuenta y hace transferencias a la aplicación");
-            Paciente.Uses(Visa, "Consulta información de su estado de cuenta y hace transferencias a la aplicación");
-
-            Psicologo.Uses(Yape, "Consulta información de su estado de cuenta para confirmar que se le halla pagado");
-            Psicologo.Uses(Plin, "Consulta información de su estado de cuenta para confirmar que se le halla pagado");
-            Psicologo.Uses(Tunki, "Consulta información de su estado de cuenta para confirmar que se le halla pagado");
-            Psicologo.Uses(Visa, "Consulta información de su estado de cuenta para confirmar que se le halla pagado");
+            Enlazador.Uses(GoogleMeet, "Realiza videoconferencias con los pacientes");
+            Enlazador.Uses(GoogleCalendar, "Agenda citas con los pacientes");
+            Enlazador.Uses(GoogleAccount, "Gestiona las cuentas de correo electrónico de los pacientes");
+            Enlazador.Uses(ChatBot, "Interactúa con los pacientes de forma natural");
 
             // Tags
             Paciente.AddTags("Paciente");
             Psicologo.AddTags("Psicologo");
+            ChatBot.AddTags("ChatBot");
             Enlazador.AddTags("NunaThani");
             Yape.AddTags("Yape");
             Plin.AddTags("Plin");
             Tunki.AddTags("Tunki");
             Visa.AddTags("Visa");
+            GoogleMeet.AddTags("GoogleMeet");
+            GoogleCalendar.AddTags("GoogleCalendar");
+            GoogleAccount.AddTags("GoogleAccount");
 
             Styles styles = viewSet.Configuration.Styles;
             styles.Add(new ElementStyle("Paciente") { Background = "#0a60ff", Color = "#ffffff", Shape = Shape.Person });
             styles.Add(new ElementStyle("Psicologo") { Background = "#aa60af", Color = "#ffffff", Shape = Shape.Person });
             styles.Add(new ElementStyle("NunaThani") { Background = "#008f39", Color = "#ffffff", Shape = Shape.RoundedBox });
-            styles.Add(new ElementStyle("Yape") { Background = "#00eaff", Color = "#ffffff", Shape = Shape.RoundedBox });
+            styles.Add(new ElementStyle("Yape") { Background = "#00193b", Color = "#ffffff", Shape = Shape.RoundedBox });
             styles.Add(new ElementStyle("Plin") { Background = "#00eaff", Color = "#ffffff", Shape = Shape.RoundedBox });
             styles.Add(new ElementStyle("Tunki") { Background = "#f700ff", Color = "#ffffff", Shape = Shape.RoundedBox });
             styles.Add(new ElementStyle("Visa") { Background = "#0008ff", Color = "#ffffff", Shape = Shape.RoundedBox });
+            styles.Add(new ElementStyle("GoogleMeet") { Background = "#ff0000", Color = "#ffffff", Shape = Shape.RoundedBox });
+            styles.Add(new ElementStyle("GoogleCalendar") { Background = "#ff0000", Color = "#ffffff", Shape = Shape.RoundedBox });
+            styles.Add(new ElementStyle("GoogleAccount") { Background = "#ff0000", Color = "#ffffff", Shape = Shape.RoundedBox });
+            styles.Add(new ElementStyle("ChatBot") { Background = "#aa60af", Color = "#ffffff", Shape = Shape.Person });
 
             SystemContextView contextView = viewSet.CreateSystemContextView(Enlazador, "Contexto", "Diagrama de contexto");
             contextView.PaperSize = PaperSize.A4_Landscape;
@@ -77,39 +83,35 @@ namespace c4_model_design
 
             // 2. Diagrama de Contenedores
             Container mobileApplication = Enlazador.AddContainer("Mobile App", "Permite a los usuarios ver sus próximas citas, tratar sus problemas de salud mental, y editar sus datos", "Swift UI");
-            Container webApplication = Enlazador.AddContainer("Web App", "Permite a los usuarios ver sus próximas citas, tratar sus problemas de salud mental, y editar sus datos", "React");
             Container landingPage = Enlazador.AddContainer("Landing Page", "", "React");
             Container apiRest = Enlazador.AddContainer("API REST", "API Rest", "NodeJS (NestJS) port 8080");
 
             Container PaymentMethod = Enlazador.AddContainer("Payment Context", "Bounded Context de cobro y pago de la aplicación, para que los pacientes agenden citas", "NodeJS (NestJS)");
-            Container CalendarContext = Enlazador.AddContainer("Calendar Context", "Bounded Context de calendarios de psicólogos y pacientes", "NodeJS (NestJS)");
+            Container AppointmentContext = Enlazador.AddContainer("Appointment Context", "Bounded Context de citas de psicólogos y pacientes", "NodeJS (NestJS)");
             Container ChatBotContext = Enlazador.AddContainer("Chatbot Context", "Bounded Context de Chatbot con el paciente", "NodeJS (NestJS)");
             Container AccountContext = Enlazador.AddContainer("Account Context", "Bounded Context de Cuentas de la aplicación", "NodeJS (NestJS)");
-            Container MentalHealthContext = Enlazador.AddContainer("Mental Illness Diagnosis Context", "Bounded Context de diagnósticos de salud mental", "NodeJS (NestJS)");
+            Container MentalHealthContext = Enlazador.AddContainer("Mental Health Diagnosis Context", "Bounded Context de diagnósticos de salud mental", "NodeJS (NestJS)");
             Container GroupsContext = Enlazador.AddContainer("Mental Care Groups Context", "Bounded Context de grupos de pacientes con enfermedades mentales", "NodeJS (NestJS)");
 
             Container database = Enlazador.AddContainer("Database", "", "SQL");
             
             Paciente.Uses(mobileApplication, "Consulta");
-            Paciente.Uses(webApplication, "Consulta");
             Paciente.Uses(landingPage, "Consulta");
 
             Psicologo.Uses(mobileApplication, "Consulta");
-            Psicologo.Uses(webApplication, "Consulta");
             Psicologo.Uses(landingPage, "Consulta");
 
             mobileApplication.Uses(apiRest, "API Request", "JSON/HTTPS");
-            webApplication.Uses(apiRest, "API Request", "JSON/HTTPS");
 
             apiRest.Uses(PaymentMethod, "", "");
-            apiRest.Uses(CalendarContext, "", "");
+            apiRest.Uses(AppointmentContext, "", "");
             apiRest.Uses(ChatBotContext, "", "");
             apiRest.Uses(AccountContext, "", "");
             apiRest.Uses(MentalHealthContext, "", "");
             apiRest.Uses(GroupsContext, "", "");
 
             PaymentMethod.Uses(database, "", "");
-            CalendarContext.Uses(database, "", "");
+            AppointmentContext.Uses(database, "", "");
             ChatBotContext.Uses(database, "", "");
             AccountContext.Uses(database, "", "");
             MentalHealthContext.Uses(database, "", "");
@@ -119,10 +121,17 @@ namespace c4_model_design
             PaymentMethod.Uses(Plin, "API Request", "JSON/HTTPS");
             PaymentMethod.Uses(Tunki, "API Request", "JSON/HTTPS");
             PaymentMethod.Uses(Visa, "API Request", "JSON/HTTPS");
+            
+            AppointmentContext.Uses(GoogleMeet, "API Request", "JSON/HTTPS");
+            AppointmentContext.Uses(GoogleCalendar, "API Request", "JSON/HTTPS");
+            
+            ChatBotContext.Uses(ChatBot, "API Request", "JSON/HTTPS");
+            
+            AccountContext.Uses(GoogleAccount, "API Request", "JSON/HTTPS");
+            
 
             // Tags
             mobileApplication.AddTags("MobileApp");
-            webApplication.AddTags("WebApp");
             landingPage.AddTags("LandingPage");
             apiRest.AddTags("APIRest");
             database.AddTags("Database");
@@ -130,14 +139,13 @@ namespace c4_model_design
             string contextTag = "Context";
 
             PaymentMethod.AddTags(contextTag);
-            CalendarContext.AddTags(contextTag);
+            AppointmentContext.AddTags(contextTag);
             ChatBotContext.AddTags(contextTag);
             AccountContext.AddTags(contextTag);
             MentalHealthContext.AddTags(contextTag);
             GroupsContext.AddTags(contextTag);
 
             styles.Add(new ElementStyle("MobileApp") { Background = "#9d33d6", Color = "#ffffff", Shape = Shape.MobileDevicePortrait, Icon = "" });
-            styles.Add(new ElementStyle("WebApp") { Background = "#9d33d6", Color = "#ffffff", Shape = Shape.WebBrowser, Icon = "" });
             styles.Add(new ElementStyle("LandingPage") { Background = "#929000", Color = "#ffffff", Shape = Shape.WebBrowser, Icon = "" });
             styles.Add(new ElementStyle("APIRest") { Shape = Shape.RoundedBox, Background = "#0000ff", Color = "#ffffff", Icon = "" });
             styles.Add(new ElementStyle("Database") { Shape = Shape.Cylinder, Background = "#ff0000", Color = "#ffffff", Icon = "" });
@@ -180,13 +188,17 @@ namespace c4_model_design
             ComponentView componentView = viewSet.CreateComponentView(PaymentMethod, "Components", "Component Diagram");
             componentView.PaperSize = PaperSize.A4_Landscape;
             componentView.Add(mobileApplication);
-            componentView.Add(webApplication);
             componentView.Add(apiRest);
             componentView.Add(database);
             componentView.Add(Visa);
             componentView.Add(Yape);
             componentView.Add(Tunki);
             componentView.Add(Plin);
+            componentView.Add(GoogleAccount);
+            componentView.Add(GoogleCalendar);
+            componentView.Add(GoogleMeet);
+            componentView.Add(ChatBot);
+            
             componentView.AddAllComponents();
 
             structurizrClient.UnlockWorkspace(workspaceId);
